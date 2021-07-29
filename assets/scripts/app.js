96ce0e572d9d6d17769b820d71e96ddb0950bacf
DebTheDev <Altine.deborah@gmail.com>
2021-07-29 09:49:51 -0400

    Emoji

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -57,7 +57,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "solve problems."
+				"am a 💻Full-Stack Web Developer.", "💓love everything about code.", "💁🏿‍♀️solve problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
@@ -65,7 +65,7 @@ const Portfolio = function() {
 			typeSpeed: 1,
 			contentType: 'text',
 			callback: function() {
-				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+				$("#writing-text").css({"color": "#fff", "background-color": "#7209b7"});
 			},
 			preStringTyped: function() {},
 			onStringTyped: function() {}
